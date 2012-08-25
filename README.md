9e4809899bb73cc17a7d18c6865449fbc208d8e2
Nick Snyder <nickdsnyder@gmail.com>
2012-08-24 23:26:42 -0700

    add example and update documentation

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 securetoken
 ===========
 
-The securetoken package implements web-safe secure tokens in Go.
+Package securetoken implements cryptographically secure tokens that provide data confidentiality and integrity.
+
+A useful application is to use securetoken to issue session cookies.
+
+Installation
+------------
+
+	go get -u github.com/nicksnyder/go-securetoken/securetoken
+
+Example
+-------
+
+	cd example/
+	go run main.go
---
+++ example/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/sha1"
+	"html/template"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/nicksnyder/go-securetoken/securetoken"
+)
+
+var unsafeKey = []byte("1234567887654321")
+var transcoder *securetoken.Transcoder
+var cookieName = "session"
+
+func main() {
+	http.HandleFunc("/", handleHome)
+	http.HandleFunc("/login", handleLogin)
+	http.HandleFunc("/logout", handleLogout)
+
+	var err error
+	transcoder, err = securetoken.NewTranscoder(unsafeKey, 24*time.Hour, sha1.New, aes.NewCipher)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("Listening on port :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+var homeTemplate = template.Must(template.New("").Parse(`
+<!DOCTYPE html>
+<html>
+	<head></head>
+	<body>
+		{{if .Email}}
+			<p>Token: {{.Token}}</p>
+			<p>You are signed in as {{.Email}}</p>
+			<form action="logout" method="POST">
+				<input type="submit" value="Logout"/>
+			</form>
+		{{else}}
+			<form action="login" method="POST">
+				Email: <input type="email" name="email" />
+				<input type="submit" value="Login"/>
+			</form>
+		{{end}}
+	</body>
+</html>
+`))
+
+func handleHome(w http.ResponseWriter, r *http.Request) {
+	c, err := r.Cookie(cookieName)
+	if err != nil {
+		homeTemplate.Execute(w, nil)
+		return
+	}
+	email, err := transcoder.Decode(c.Value)
+	if err != nil {
+		panic(err)
+	}
+	homeTemplate.Execute(w, map[string]string{
+		"Token": c.Value,
+		"Email": string(email),
+	})
+}
+
+func handleLogin(w http.ResponseWriter, r *http.Request) {
+	email := r.FormValue("email")
+	token, err := transcoder.Encode([]byte(email))
+	if err != nil {
+		panic(err)
+	}
+	http.SetCookie(w, &http.Cookie{
+		Name:     cookieName,
+		Value:    token,
+		HttpOnly: true,
+	})
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+func handleLogout(w http.ResponseWriter, r *http.Request) {
+	http.SetCookie(w, &http.Cookie{
+		Name:     cookieName,
+		Expires:  time.Unix(1, 0),
+		HttpOnly: true,
+	})
+	http.Redirect(w, r, "/", http.StatusFound)
+}
